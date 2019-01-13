1a71c83a1dc08126cddad82d35f32803dc510178
jiangqianghua <240437339@qq.com>
2019-01-13 10:56:19 +0800

    添加优惠券dao，拦截器等

---
+++ src/main/java/com/jqh/passbook/constant/Constants.java
@@ -0,0 +1,21 @@
+package com.jqh.passbook.constant;
+
+/**
+ * <h1>普通（通用的常量定义）</h1>
+ */
+public class Constants {
+    /**
+     * 商户优惠券投放的Kafka Topic
+     */
+    public static final String TEMPLATE_TOPIC = "merchants-template";
+
+    /**
+     * token string
+     */
+    public static final String  TOKEN_STRING = "token";
+
+    /**
+     *token info
+     */
+    public static final String TOKEN = "jqh-passbook-merchants";
+}
---
+++ src/main/java/com/jqh/passbook/dao.java
@@ -0,0 +1,10 @@
+package com.jqh.passbook;
+
+/**
+ * @Author: jiang qiang hua
+ * @Description:<h1></h1>
+ * @Date: Create in 10:22 2019/1/13
+ * @Modified By:
+ **/
+public class dao {
+}
